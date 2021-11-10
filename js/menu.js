58090c1dd123e9a0d7b571a141391866c53c6521
Kesha2603 <84954001+Kesha2603@users.noreply.github.com>
2021-11-10 23:22:22 +0300

    Delivery-Food-JS
    Delivery-Food-JS

---
+++ js/menu.js
@@ -0,0 +1,91 @@
+const menu = () => {
+    const cardsMenu = document.querySelector('.cards-menu')
+
+    const cartArray = localStorage.getItem('cart') 
+        ? JSON.parse(localStorage.getItem('cart')) 
+        : []
+
+
+    const changeTitle = ({stars, price, kitchen, name}) => {
+        const restaurantTitle = document.querySelector('.restaurant-title')
+        const rating = document.querySelector('.rating')
+        const cena = document.querySelector('.price')
+        const category = document.querySelector('.category')
+        
+        restaurantTitle.textContent = name
+        rating.textContent = stars
+        cena.textContent = `От ${price} ₽`
+        category.textContent = kitchen
+    }
+
+    const addToCart = (cartItem) => {
+        if (cartArray.some((item) => item.id === cartItem.id)) {
+            cartArray.map((item) => {
+                if (item.id === cartItem.id) {
+                    item.count++
+                }
+
+                return item
+            })
+        } else {
+            cartArray.push(cartItem)
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cartArray))
+    }
+
+    const renderItems = (data) => {
+        data.forEach(({ description, id, image, name, price }) => {
+            const card = document.createElement('div')
+            
+            card.classList.add('card')
+
+            // ` ` - это backtiti или интерполяция
+            card.innerHTML = `
+                <img src="${image}" alt="${name}" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title card-title-reg">${name}</h3>
+                    </div>
+                    <div class="card-info">
+                        <div class="ingredients">
+                            ${description}
+                        </div>
+                    </div>
+                    <div class="card-buttons">
+                        <button class="button button-primary button-add-cart">
+                            <span class="button-card-text">В корзину</span>
+                            <span class="button-cart-svg"></span>
+                        </button>
+                        <strong class="card-price-bold">${price} ₽</strong>
+                    </div>
+                </div>
+            `
+
+            card.querySelector('.button-card-text').addEventListener('click', () => {
+                addToCart({ name, price, id, count: 1 })
+            })
+
+            cardsMenu.append(card)
+        })
+    }
+
+    if (localStorage.getItem('restaurant')) {
+        const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+
+        changeTitle(restaurant)
+        
+        fetch(`./db/${restaurant.products}`)
+        .then(response => response.json())
+        .then((data) => {
+            renderItems(data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    } else {
+        window.location.href = '/'
+    }
+}
+
+menu()
